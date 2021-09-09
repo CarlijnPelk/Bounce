0690279249ee3e68dc35e992d82ef0e4293d2c36
CarlijnPelk <carlijnpelk@hotmail.com>
2021-09-09 17:50:04 +0000

    9/09 ballen bewegen

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+// globale variabelen
+var x = 640;
+var y = 360;
+var speedX = Math.floor((Math.random() * 10) - 10);
+var speedY = Math.floor((Math.random() * 10) - 10);
+
 /**
  * setup
  * de code in deze functie wordt één keer uitgevoerd door
@@ -17,10 +23,39 @@ function setup() {
  * de code in deze functie wordt meerdere keren per seconde
  * uitgevoerd door de p5 library, nadat de setup functie klaar is
  */
-function draw() {
-  // stel vulkleur in
-  fill(100, 100, 255);
+ function draw() {
 
-  // teken een cirkel
-  ellipse(50,50,80,80);
+  
+  // Kleur de achtergrond blauw, zodat je het kunt zien
+  background('blue');
+
+  // stel vulkleur in
+  fill(255, 255, 255);
+
+  for (var i = 0; i < 5; i++){   
+    // teken een cirkel
+    ellipse(x, y, 80, 80);
+
+  
+    //positie updaten
+    x = x + speedX;
+    y = y + speedY;
+  }
+
+  // laat stuiteren tegen de onderkant
+  if (y === 720) {
+    speedY = speedY * -1;
+  }
+
+  if (y === 0) {
+    speedY = speedY * -1;
+  }
+
+  if (x === 1280) {
+    speedX = speedX * -1;
+  }
+
+  if (x === 0) {
+    speedX = speedX * -1;
+  }
 }
